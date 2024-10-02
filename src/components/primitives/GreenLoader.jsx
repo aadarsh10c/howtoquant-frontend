f71a345c8c144e908810ae032016ea8adf36a057
adarsh010c <adarsh010c@gmail.com>
2024-10-02 11:30:33 +0530

    removed spinner-reac, added pure css loader

---
+++ src/components/primitives/GreenLoader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../assets/css/Loader.css';
+
+
+function GreenLoader(){
+  return (
+    <div className='spinner' ></div>
+  )
+}
+
+export default GreenLoader
